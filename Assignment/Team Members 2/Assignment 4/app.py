fbc05c1856592865b4713d19f10d338d8e9cb647
Niketha-30 <113673414+Niketha-30@users.noreply.github.com>
2022-11-14 14:35:59 +0530

    Add files via upload

---
+++ Assignment/Team Members 2/Assignment 4/app.py
@@ -0,0 +1,15 @@
+import uuid
+from flask import Flask
+
+instance_id = uuid.uuid4().hex
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_instance_id():
+    return f"<b style='font-size:30px;color:red;'>Instance ID: {instance_id}</b>"
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
