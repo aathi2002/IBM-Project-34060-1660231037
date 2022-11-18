87b0d81659e9657e91c3b3721ed5ce40b1234492
Athi guru k <68262020+aathi2002@users.noreply.github.com>
2022-11-18 21:47:46 +0530

    Add the sprint 1 to the project

---
+++ Project Development phase/Sprint 1/SPRINT PHASE 1/app.py
@@ -0,0 +1,135 @@
+#IMPORT REQUIRED LIBRARY  FOR OUR PROJECT
+from flask import Flask, render_template, request, redirect, session 
+import re
+from flask_db2 import DB2
+import ibm_db
+import ibm_db_dbi
+app = Flask(__name__)
+#DATABASE CONNECTION
+try:
+
+    conn_str='DATABASE=bludb;HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32731;protocol=tcpip;UID=dwy98106;PWD=4tGfLvXSdE60jCTe;security=ssl'
+
+    ibm_db_conn = ibm_db.connect(conn_str,'','')
+        
+    print("Database connected without any error !!")
+except:
+    print("IBM DB Connection error   :     " + DB2.conn_errormsg())    
+
+#HOME--PAGE
+@app.route("/home")
+def home():
+    return render_template("homepage.html")
+
+@app.route("/")
+def add():
+    return render_template("home.html")
+
+
+
+#SIGN--UP--OR--REGISTER
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    print("Break point1")
+    if request.method == 'POST' :
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        print("Break point2" + "name: " + username + "------" + email + "------" + password)
+
+        try:
+            print("Break point3")
+            connectionID = ibm_db_dbi.connect(conn_str, '', '')
+            cursor = connectionID.cursor()
+            print("Break point4")
+        except:
+            print("No connection Established")      
+
+        
+        print("Break point5")
+        sql = "SELECT * FROM register WHERE username = ?"
+        stmt = ibm_db.prepare(ibm_db_conn, sql)
+        ibm_db.bind_param(stmt, 1, username)
+        ibm_db.execute(stmt)
+        result = ibm_db.execute(stmt)
+        print(result)
+        account = ibm_db.fetch_row(stmt)
+        print(account)
+
+        param = "SELECT * FROM register WHERE username = " + "\'" + username + "\'"
+        res = ibm_db.exec_immediate(ibm_db_conn, param)
+        print("---- ")
+        dictionary = ibm_db.fetch_assoc(res)
+        while dictionary != False:
+            print("The ID is : ", dictionary["USERNAME"])
+            dictionary = ibm_db.fetch_assoc(res)
+        print("break point 6")
+        if account:
+            msg = 'Username already exists Please Login !!!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'name must contain only characters and numbers !'
+        else:
+            sql2 = "INSERT INTO register (username, email,password) VALUES (?, ?, ?)"
+            stamt = ibm_db.prepare(ibm_db_conn, sql2)
+            ibm_db.bind_param(stamt, 1, username)
+            ibm_db.bind_param(stamt, 2, email)
+            ibm_db.bind_param(stamt, 3, password)
+            ibm_db.execute(stamt)
+            msg = 'You have successfully registered !'
+        return render_template('login.html', msg = msg)
+        
+        
+ #LOGIN--PAGE
+    
+@app.route("/signin")
+def signin():
+    return render_template("login.html")
+        
+@app.route('/login',methods =['GET', 'POST'])
+def login():
+    global userid
+    msg = ''
+   
+  
+    if request.method == 'POST' :
+        username = request.form['username']
+        password = request.form['password']
+        sql = "SELECT * FROM register WHERE username = ? and password = ?"
+        stmt = ibm_db.prepare(ibm_db_conn, sql)
+        ibm_db.bind_param(stmt, 1, username)
+        ibm_db.bind_param(stmt, 2, password)
+        result = ibm_db.execute(stmt)
+        print(result)
+        account = ibm_db.fetch_row(stmt)
+        print(account)       
+        param = "SELECT * FROM register WHERE username = " + "\'" + username + "\'" + " and password = " + "\'" + password + "\'"
+        res = ibm_db.exec_immediate(ibm_db_conn, param)
+        dictionary = ibm_db.fetch_assoc(res)
+
+        if account:
+            session['loggedin'] = True
+            session['id'] = dictionary["ID"]
+            userid = dictionary["ID"]
+            session['username'] = dictionary["USERNAME"]
+            session['email'] = dictionary["EMAIL"]
+           
+            return redirect('/home')
+        else:
+            msg = 'Incorrect username / password !'
+        
+    return render_template('login.html', msg = msg)
+                 
+if __name__ == "__main__":
+    app.run(debug=True)
